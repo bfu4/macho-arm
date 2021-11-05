68c64aa6f2b9a494e197cb1205f05a0d88e8b38e
bfu4 <bella@sleeeepyti.me>
2021-11-04 22:23:21 -0400

    both versions

--- return_42/fourtytwo.s
+++ return_42/fourtytwo.s
@@ -1,9 +1,9 @@
-.align 2
+.align 4
 
-.globl _start
-
-_start:
-	mov 	x16, #1
-	eor	x0, x0, x0
-	mov	x0, #42
-	svc 	#0x80
+.dword 0xfeedfacf	; macho64 magic
+.dword 0x0100000c	; cpu type (arm64)
+.dword 0x0		; cpu subtype
+.dword 0x00000002	; type: mh_execute
+.dword 0x00000002	; ncmds
+.dword 0x10		; todo(bfu4): cmd size
+.dword 0x01		; flags
